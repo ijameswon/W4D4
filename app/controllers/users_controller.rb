9ab8188f512d2091149600044d3052ad50cbb3d7
james_won <ijameswon@gmail.com>
2017-09-28 16:53:13 -0400

    music_project

---
+++ app/controllers/users_controller.rb
@@ -0,0 +1,30 @@
+class UsersController < ApplicationController
+
+  def new
+    @user = User.new
+    render :new
+  end
+
+  def show
+    render :show
+  end
+
+  def create
+    @user = User.create(user_params)
+
+    if @user.save
+      login_user!(@user)
+      redirect_to root_url
+    else
+      flash[:errors] = 'Invalid email/password'
+      render :newx
+    end
+  end
+
+  private
+
+  def user_params
+    params.require(:user).permit(:email, :password)
+  end
+
+end
